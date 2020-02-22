69be69caeabf256a2045bf75a734f343f8cbec1a
Emilcan <emilcan.98@gmail.com>
2020-02-22 22:17:56 +0300

    cosmetic changes
    exception messages passed as a variable

--- bal/transaction_pool.py
+++ bal/transaction_pool.py
@@ -13,21 +13,20 @@ class TransactionPool:
 
     def add_to_transaction_pool(self, tx, unspent_tx_outs):
         if not validate_transaction(tx, unspent_tx_outs):
-            raise Exception(
-                'Trying to add invalid tx to pool: ' +
-                json.dumps(tx))
+            exception_message = 'Trying to add invalid tx to pool: ' + json.dumps(tx)
+            raise Exception(exception_message)
 
         if not self.is_valid_tx_for_pool(tx):
-            raise Exception('Trying to add same tx to pool')
+            exception_message = 'Trying to add same tx to pool'
+            raise Exception(exception_message)
 
         print('adding to txPool: %s', json.dumps(tx))
         self.transaction_pool.append(tx.copy())
 
     def has_tx_in(self, tx_in, unspent_tx_outs):
-        found_tx_in = find_unspent_tx_out(
-            tx_in['tx_out_id'],
-            tx_in['tx_out_index'],
-            unspent_tx_outs)
+        found_tx_in = find_unspent_tx_out(tx_in['tx_out_id'], 
+                                          tx_in['tx_out_index'], 
+                                          unspent_tx_outs)
         return bool(found_tx_in)
 
     def update_transaction_pool(self, unspent_tx_outs):
@@ -38,16 +37,14 @@ class TransactionPool:
                     invalid_txs.append(tx)
                     break
         if len(invalid_txs) > 0:
-            print(
-                'removing the following transactions from txPool: %s',
-                json.dumps(invalid_txs))
-            self.transaction_pool = [
-                tx for tx in self.transaction_pool if tx not in invalid_txs]
+            print('removing the following transactions from txPool: %s', 
+                  json.dumps(invalid_txs))
+            self.transaction_pool = [tx for tx in self.transaction_pool if tx not in invalid_txs]
 
     def get_tx_pool_ins(self):
-        return seq(self.transaction_pool)\
-            .map(lambda tx: tx['tx_ins'])\
-            .flatten()
+        return (seq(self.transaction_pool)
+                .map(lambda tx: tx['tx_ins'])
+                .flatten())
 
     def is_valid_tx_for_pool(self, tx):
         tx_pool_ins = self.get_tx_pool_ins()
