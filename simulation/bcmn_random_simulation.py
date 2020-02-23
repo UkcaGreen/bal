199336f3c5e5dbc4d6cae843db39e055dfc00829
Emilcan <emilcan.98@gmail.com>
2020-02-23 13:00:07 +0300

    cosmetic changes

--- simulation/bcmn_random_simulation.py
+++ simulation/bcmn_random_simulation.py
@@ -26,15 +26,10 @@ from bcmn_simulation import *
 from simulation_tools import *
 
 
-def simulate(
-        host_type,
-        host_number,
-        max_bw,
-        miner_percentages,
-        transaction_counts,
-        simulation_count,
-        root_path,
-        debug_mode):
+def simulate(host_type, host_number, 
+             max_bw, miner_percentages, 
+             transaction_counts, simulation_count,
+             root_path, debug_mode):
     switch_number = host_number / 4 or 1
     edge_number = switch_number
 
@@ -49,41 +44,35 @@ def simulate(
         'waitConnected': True,
         'xterms': debug_mode}
 
-    for miner_percentage, transaction_count in itertools.product(
-            miner_percentages, transaction_counts):
+    for miner_percentage, transaction_count \
+        in itertools.product(miner_percentages, transaction_counts):
+        
         for i in range(0, simulation_count):
             print("Simulating with miner %: " + str(miner_percentage)
                   + ", transaction #: " + str(transaction_count))
-            parametered_path = 'h' + \
-                str(host_number) + 'm' + str(miner_percentage) + 't' + str(transaction_count)
+            parametered_path = ('h' + str(host_number) + 
+                                'm' + str(miner_percentage) + 
+                                't' + str(transaction_count))
             path = root_path + parametered_path
 
             miner_number = (len(topo.hosts()) * miner_percentage / 100) or 1
             miner_names = random.sample(topo.hosts(), miner_number)
-            transactor_names = [
-                random.choice(
-                    topo.hosts()) for x in range(transaction_count)]
-            subsimulation(
-                net_params,
-                miner_names,
-                transactor_names,
-                path,
-                debug_mode)
+            transactor_names = [random.choice(topo.hosts()) for x 
+                                in range(transaction_count)]
+            subsimulation(net_params, miner_names, 
+                          transactor_names, path, 
+                          debug_mode)
 
-
-def subsimulation(
-        net_params,
-        miner_names,
-        transactor_names,
-        sim_path,
-        debug_mode):
+def subsimulation(net_params, miner_names,
+                  transactor_names, sim_path,
+                  debug_mode):
     net = None
     try:
         start_time = time()
         timestamp_str = str(int(start_time))
 
-        ts_dir_path = init_simulation_path(
-            sim_path + '/' + timestamp_str + '/')
+        ts_dir_path = init_simulation_path(sim_path + '/' + 
+                                           timestamp_str + '/')
 
         net = Mininet(**net_params)
         net.build()
@@ -118,16 +107,10 @@ def subsimulation(
                 if h.name in generated:
                     continue
                 host_amount = verifier_check_amount(h, verifier)
-                print(
-                    h.name +
-                    ' has ' +
-                    str(host_amount) +
-                    ' coins currently, target is: ' +
-                    str(target_amount))
+                print(h.name + ' has ' + str(host_amount) + ' coins currently, ' + 
+                      'target is: ' + str(target_amount))
                 if (host_amount >= target_amount):
-                    print(
-                        h.name +
-                        ' has enough coins, stopping generation for it')
+                    print(h.name +' has enough coins, stopping generation for it')
                     h.call('block/generate/loop/stop', True)
                     generated.append(h.name)
 
@@ -139,12 +122,10 @@ def subsimulation(
         while i < transaction_count:
             sender = random.choice(transactors)
             receiver = random.choice([n for n in net.hosts if n != sender])
-            current_block_number = yaml.safe_load(
-                sender.call('chain/length', True))
+            current_block_number = yaml.safe_load(sender.call('chain/length', True))
             while current_block_number <= temp_block_number:  # wait for next block to be forged before new tx
                 sleep(BLOCK_GENERATION_INTERVAL)
-                current_block_number = yaml.safe_load(
-                    sender.call('chain/length', True))
+                current_block_number = yaml.safe_load(sender.call('chain/length', True))
 
             if send_and_log_transaction(sender, receiver, 1, ts_dir_path):
                 i = i + 1
@@ -154,10 +135,9 @@ def subsimulation(
 
         print('Waiting for nodes to receive transactions')
         host_number = len(net.hosts)
-        while not check_block_txts(
-                ts_dir_path,
-                host_number,
-                transaction_count):
+        while not check_block_txts(ts_dir_path, 
+                                   host_number, 
+                                   transaction_count):
             sleep(0.5)
 
         elapsed_time = time() - start_time
@@ -175,30 +155,27 @@ def subsimulation(
 def main():
     host_type = None
     parser = ArgumentParser()
-    parser.add_argument(
-        '-ht',
-        '--host_type',
-        default='pow',
-        type=str,
-        help='blockchain consensus class to be used')
-    parser.add_argument(
-        '-p',
-        '--path',
-        default='/tmp/',
-        type=str,
-        help='where the logs will be located. default: /tmp/')
-    parser.add_argument(
-        '-d',
-        '--debug',
-        default=False,
-        help='debug mode for xterms.',
-        action='store_true')
-    parser.add_argument(
-        '-s',
-        '--setup',
-        default=False,
-        help='go with setup mode for h=10,20,50,100 m=10,20,50,80 tx=10, sim=10',
-        action='store_true')
+        
+    help_message = 'blockchain consensus class to be used'
+    parser.add_argument('-ht', '--host_type', 
+                        default='pow', type=str, 
+                        help=help_message)
+        
+    help_message = 'where the logs will be located. default: /tmp/'
+    parser.add_argument('-p', '--path', 
+                        default='/tmp/', type=str, 
+                        help=help_message)
+        
+    help_message = 'debug mode for xterms.'
+    parser.add_argument('-d', '--debug',
+                        default=False, help=help_message,
+                        action='store_true')
+        
+    help_message = 'go with setup mode for h=10,20,50,100 m=10,20,50,80 tx=10, sim=10'
+    parser.add_argument('-s', '--setup',
+                        default=False, help=help_message,
+                        action='store_true')
+        
     args = parser.parse_args()
     tmp_location = '/tmp/bcn'
     if os.path.exists(tmp_location):
@@ -218,15 +195,10 @@ def main():
         simulation_count = 5
         max_bw = 100
         for host_number in host_numbers:
-            simulate(
-                host_type,
-                host_number,
-                max_bw,
-                miner_percentages,
-                transaction_counts,
-                simulation_count,
-                args.path,
-                args.debug)
+            simulate(host_type, host_number, 
+                     max_bw, miner_percentages, 
+                     transaction_counts, simulation_count, 
+                     args.path, args.debug)
 
     else:
         host_number = int(input("Number of hosts(>10):"))
@@ -235,15 +207,10 @@ def main():
         transaction_count = int(
             input("Number of repeated random transactions:"))
         simulation_count = int(input("Number of repeated simulations:"))
-        simulate(
-            host_type,
-            host_number,
-            max_bw,
-            [miner_percentage],
-            [transaction_count],
-            simulation_count,
-            args.path,
-            args.debug)
+        simulate(host_type, host_number, 
+                 max_bw, [miner_percentage], 
+                 [transaction_count], simulation_count,
+                 args.path, args.debug)
 
 
 if __name__ == '__main__':
